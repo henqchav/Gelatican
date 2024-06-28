3e59c18d4506c4da472c59dedcee11f3fb25d026
henqchav <henqchav@espol.edu.ec>
2024-06-27 22:49:36 -0500

    new adapter changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://henqchav.github.io',
   base: 'Gelatican',
   output: 'hybrid', 
-  integrations: [tailwind()]
+  integrations: [tailwind(), netlify()]
 });
