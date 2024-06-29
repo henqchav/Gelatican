af177daaa1ce9375d2777ddb60b866b6a466d82c
henqchav <henqchav@espol.edu.ec>
2024-06-29 11:17:46 -0500

    versel adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://henqchav.github.io',
   base: 'Gelatican',
-  output: 'hybrid', 
+  output: 'server', 
   integrations: [tailwind()],
   adapters: vercel()
 });
