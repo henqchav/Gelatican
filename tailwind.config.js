b9e3c92ba34f06fd9d8fd0af65c1e40c3ed6e681
henqchav <henqchav@espol.edu.ec>
2024-07-01 08:25:34 -0500

    css added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./Src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  fontFamily: {
+			'custom': ['Noto Sans', 'Open Sans'],
+		  },
+		  colors: {
+			'custom-green': '#85cc9c',
+			'soft-yellow': '#fafad7',
+			'blue1': '#2b9eb3',
+		  },
+		},
+		
+	  },
+	plugins: [],
+}
+
