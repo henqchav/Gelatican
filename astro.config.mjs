c6ab693744f7714adfc90a4b57983c7ae82587b7
henqchav <henqchav@espol.edu.ec>
2024-07-01 09:26:18 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://henqchav.github.io',
   base: 'Gelatican',
   output: 'hybrid', 
-  integrations: [tailwind(), vercel()],
-  adapters: vercel()
+  integrations: [tailwind(), netlify()],
+  adapters: netlify()
 });
