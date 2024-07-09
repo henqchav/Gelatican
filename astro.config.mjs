c09d18cb43d40c97bf0ba2c1a2deaba7ff0ffadd
henqchav <93171012+henqchav@users.noreply.github.com>
2024-07-09 12:50:25 -0500

    package update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://henqchav.github.io',
-  base: 'Gelatican',
+  base: '/',
   output: 'hybrid', 
   integrations: [tailwind(), netlify()],
   adapters: netlify()
